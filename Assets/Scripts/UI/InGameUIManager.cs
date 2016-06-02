a1a38ba02a4e7cdf827df6f1300c33bbb9736b87
L0stW0lf <lostwolf@gmx.net>
2016-06-02 19:28:31 +0200

    InGameUI 0.1

---
+++ Assets/Scripts/UI/InGameUIManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InGameUIManager : MonoBehaviour {
+
+    public GameObject escMenuPanel; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            escMenuPanel.SetActive(!escMenuPanel.activeSelf);
+        }
+    }
+
+    public void ExitButton()
+    {
+
+    }
+
+    public void DisconnectButton()
+    {
+
+    }
+
+
+
+}
